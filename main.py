9e0eef50348ec7f6265e0675e1c9701faa82bbe1
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2023-06-22 11:19:38 +0200

    used sqlalchemy
    Here I have used SQLALCHEMY to add my favorite book harry potter

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-all_books = []
+##CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+# Optional: But it will silence the deprecation warning in the console.
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def home():
-    return render_template("index.html", books=all_books)
+##CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
 
 
-@app.route("/add", methods=['POST', 'GET'])
-def add():
-    if request.method == 'POST':
-        all_books.append(request.form.to_dict())
-        # redirects to another route like home page after the form is submitted
-        return redirect(url_for('home'))
-    return render_template('add.html')
+db.create_all()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# CREATE RECORD
+new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+db.session.add(new_book)
+db.session.commit()
